079ddec0f106328af213a76fa7425c02e0a08217
haze.liu <liuxyuan@cn.ibm.com>
2024-10-12 12:14:44 +0800

    fix(components): [file-dragger]

--- packages/components/file-dragger/src/propsAndEmit.ts
+++ packages/components/file-dragger/src/propsAndEmit.ts
@@ -1,4 +1,4 @@
-import { UPDATE_MODEL_EVENT } from '@setaria-business-framework/constants'
+// import { UPDATE_MODEL_EVENT } from '@setaria-business-framework/constants'
 import type { ExtractPropTypes } from 'vue'
 export type InputValueType = string
 export const fileDraggerProps = {
